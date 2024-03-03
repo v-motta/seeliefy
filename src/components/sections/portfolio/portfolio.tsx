ed9d3a9151654c0ee818bbc3dd7b8453804784db
v-motta <viniciusmotta0806@gmail.com>
2024-03-03 16:44:31 -0300

    bug fix do portifolio

--- src/components/sections/portfolio/portfolio.tsx
+++ src/components/sections/portfolio/portfolio.tsx
@@ -6,6 +6,8 @@ import { PortfolioJSON } from "./list";
 function Portifolio() {
   const [project, setProject] = useState("BBB 23 - Corrida");
 
+  const projectReplaced = project.replace(/[- ]/g, "");
+
   return (
     <>
       <h1 id="scrollspyHeading2" className="text-center">
@@ -27,29 +29,33 @@ function Portifolio() {
             </li>
           ))}
         </ul>
-        <div id="carouselExampleIndicators2" className="carousel slide">
+        <div
+          id="carouselExampleIndicators2"
+          className="carousel slide"
+          key={projectReplaced}
+        >
           <div className="carousel-indicators mb-0">
             {PortfolioJSON.filter((item) => item.projetoName === project).map(
               (item) =>
-                item.images.map((image, index) => (
+                item.images.map((_image, index) => (
                   <button
+                    key={index}
                     type="button"
                     data-bs-target="#carouselExampleIndicators2"
                     data-bs-slide-to={index}
                     className={index === 0 ? "active" : ""}
                     aria-current="true"
-                    aria-label={`Slide ${index++}`}
+                    aria-label={`Slide ${index + 1}`}
                   ></button>
                 ))
             )}
           </div>
-
-          {/* começo do carousel */}
           <div className="carousel-inner">
             {PortfolioJSON.filter((item) => item.projetoName === project).map(
               (projeto) =>
                 projeto.images.map((image, index) => (
                   <div
+                    key={index}
                     className={`carousel-item ${index === 0 ? "active" : ""}`}
                   >
                     <img
